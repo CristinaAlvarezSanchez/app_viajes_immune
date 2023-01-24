8057285941e8ed4087483b6916530541129f05ad
Cristina Álvarez Sánchez <alvarezsanchezc@gmail.com>
2023-01-24 23:36:29 +0100

    actualizado modelo entidad relación MSQL

--- bd/app_viajes.sql
+++ bd/app_viajes.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `app_viajes` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `app_viajes`;
 -- MySQL dump 10.13  Distrib 8.0.30, for macos12 (x86_64)
 --
 -- Host: localhost    Database: app_viajes
@@ -36,7 +34,7 @@ CREATE TABLE `clientes` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`),
   UNIQUE KEY `dni_UNIQUE` (`dni`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -45,7 +43,7 @@ CREATE TABLE `clientes` (
 
 LOCK TABLES `clientes` WRITE;
 /*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
-INSERT INTO `clientes` VALUES (1,'María','Villar','Calle Colmenar 34 ','mariavillar@no.com','445557765','1972-05-02','11111134D'),(2,'Kelci','Parkinson','Apt 1494','cbodiam3@bandcamp.com','8414723309','1978-05-02','753218037L'),(3,'Gerhardine','Lichtfoth','Apt 1290','glichtfoth0@nih.gov','4321178392','1970-12-24','750'),(4,'Umeko','Acreman','Room 266','uacreman5@washington.edu','4086010654','1980-06-07','2660661'),(5,'Verene','Endean','Suite 14','vendean6@alibaba.com','9009585062','1985-01-25','0342958'),(6,'Malorie','McWhirter','Suite 19','mmcwhirter7@a8.net','1674202860','1970-01-29','7528780'),(7,'Wrennie','O\'Heaney','Room 1393','woheaney8@friendfeed.com','7454620867','1983-05-23','4323889'),(8,'Elberta','Feake','Apt 881','efeake9@webmd.com','9903093135','1960-11-22','3624434');
+INSERT INTO `clientes` VALUES (1,'María','Villar','Calle Colmenar 34 ','mariavillar@no.com','445557765','1972-05-02','11111134D'),(2,'Kelci','Parkinson','Apt 1494','cbodiam3@bandcamp.com','8414723309','1978-05-02','753218037L'),(3,'Gerhardine','Lichtfoth','Apt 1290','glichtfoth0@nih.gov','4321178392','1970-12-24','750'),(4,'Umeko','Acreman','Room 266','uacreman5@washington.edu','4086010654','1980-06-07','2660661'),(5,'Verene','Endean','Suite 14','vendean6@alibaba.com','9009585062','1985-01-25','0342958'),(6,'Malorie','McWhirter','Suite 19','mmcwhirter7@a8.net','1674202860','1970-01-29','7528780'),(7,'Wrennie','O\'Heaney','Room 1393','woheaney8@friendfeed.com','7454620867','1983-05-23','4323889');
 /*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -65,7 +63,7 @@ CREATE TABLE `hoteles` (
   `descripcion` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `tarifa` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -88,13 +86,13 @@ DROP TABLE IF EXISTS `tbi_clientes_viajes`;
 CREATE TABLE `tbi_clientes_viajes` (
   `id` int NOT NULL AUTO_INCREMENT,
   `fk_cliente` int NOT NULL,
-  `fk_viaje` int NOT NULL,
+  `fk_viaje` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_clientes_has_viajes_viajes1_idx` (`fk_viaje`),
   KEY `fk_clientes_has_viajes_clientes1_idx` (`fk_cliente`),
-  CONSTRAINT `fk_clientes_has_viajes_clientes1` FOREIGN KEY (`fk_cliente`) REFERENCES `clientes` (`id`),
-  CONSTRAINT `fk_clientes_has_viajes_viajes1` FOREIGN KEY (`fk_viaje`) REFERENCES `viajes` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  CONSTRAINT `fk_clientes_has_viajes_clientes1` FOREIGN KEY (`fk_cliente`) REFERENCES `clientes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_clientes_has_viajes_viajes1` FOREIGN KEY (`fk_viaje`) REFERENCES `viajes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=64 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -103,7 +101,7 @@ CREATE TABLE `tbi_clientes_viajes` (
 
 LOCK TABLES `tbi_clientes_viajes` WRITE;
 /*!40000 ALTER TABLE `tbi_clientes_viajes` DISABLE KEYS */;
-INSERT INTO `tbi_clientes_viajes` VALUES (1,1,8),(2,2,7),(3,3,6),(4,4,5),(5,5,4),(6,6,3),(7,7,2),(8,8,1);
+INSERT INTO `tbi_clientes_viajes` VALUES (3,3,6),(4,4,5),(5,5,4),(6,6,3);
 /*!40000 ALTER TABLE `tbi_clientes_viajes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -120,11 +118,11 @@ CREATE TABLE `viajes` (
   `fecha_vuelta` date NOT NULL,
   `idvuelo_ida` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
   `idvuelo_vuelta` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `hotel_id` int NOT NULL,
-  PRIMARY KEY (`id`,`hotel_id`),
+  `hotel_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
   KEY `fk_viajes_hoteles1_idx` (`hotel_id`),
   CONSTRAINT `fk_viajes_hoteles1` FOREIGN KEY (`hotel_id`) REFERENCES `hoteles` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -133,7 +131,7 @@ CREATE TABLE `viajes` (
 
 LOCK TABLES `viajes` WRITE;
 /*!40000 ALTER TABLE `viajes` DISABLE KEYS */;
-INSERT INTO `viajes` VALUES (1,'2023-07-23','2023-08-02','IB3345','IB3221',1),(2,'2023-07-18','2023-08-28','NTA7765','AGI8756',1),(3,'2023-03-29','2023-03-31','HRR7765','APO8876',2),(4,'2023-02-13','2023-03-05','AXG8767','VVB8769',3),(5,'2022-12-13','2024-02-20','RTW765','SRX8867',4),(6,'2023-07-25','2023-09-20','PCO8878','MXZ8879',4),(7,'2023-06-22','2023-07-15','KZR9898','MGC9878',1),(8,'2023-02-02','2023-03-01','IRZ8987','TEV8768',2);
+INSERT INTO `viajes` VALUES (1,'2023-04-04','2023-04-09','ib6732','new7765',4),(2,'2023-07-18','2023-08-28','NTA7765','AGI8756',1),(3,'2023-03-29','2023-03-31','HRR7765','APO8876',2),(4,'2023-02-13','2023-03-05','AXG8767','VVB8769',3),(5,'2022-12-13','2024-02-20','RTW765','SRX8867',4),(6,'2023-07-25','2023-09-20','PCO8878','MXZ8879',4),(62,'2023-04-04','2023-04-09','pru432','pru7765',NULL),(63,'2023-04-04','2023-04-09','pru432','pru7765',4);
 /*!40000 ALTER TABLE `viajes` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -146,4 +144,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-01-20 21:21:00
+-- Dump completed on 2023-01-24 23:27:03
